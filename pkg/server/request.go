330f010c2cb55a30c094e2c2f5a136ba0fe30e3f
Josh Edney <josh.edney@smartbear.com>
2022-08-09 11:30:22 +0100

    chore: add request.go pkg

---
+++ pkg/server/request.go
@@ -0,0 +1,56 @@
+package request
+
+import (
+	"bytes"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+/*
+	BuildFileRequest - Create a multi-part form request adding a file as a parameter
+*/
+func BuildFileRequest(url string,  fieldData map[string]string, fileFieldName string, fileName string) (*http.Request, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	for key, value := range fieldData {
+		writer.WriteField(key, value)
+	}
+
+	part, err := writer.CreateFormFile(fileFieldName, filepath.Base(file.Name()))
+
+
+	if err != nil {
+		return nil, err
+	}
+
+	io.Copy(part, file)
+	writer.Close()
+	request, err := http.NewRequest("POST", url, body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header.Add("Content-Type", writer.FormDataContentType())
+	return request, nil
+}
+
+func SendRequest(request *http.Request) (*http.Response, error) {
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
