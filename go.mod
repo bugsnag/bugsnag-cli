6170a068352e2f5652e1fe40089589b5cd32374b
Josh Edney <josh.edney@smartbear.com>
2022-08-09 15:42:56 +0100

    chore: update logging

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/bugsnag/bugsnag-cli
 
 go 1.18
+
+require (
+	github.com/alecthomas/kong v0.6.1
+	unknwon.dev/clog/v2 v2.2.0
+)
+
+require (
+	github.com/fatih/color v1.7.0 // indirect
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	golang.org/x/sys v0.0.0-20191008105621-543471e840be // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,58 @@
 package main
 
 import (
-	"fmt"
-	"github.com/bugsnag/bugsnag-cli/pkg/server/request"
+	"github.com/bugsnag/bugsnag-cli/pkg/server"
+	"github.com/bugsnag/bugsnag-cli/pkg/validation"
+	"github.com/alecthomas/kong"
+	"os"
+	log "unknwon.dev/clog/v2"
 )
 
+func init() {
+	err := log.NewConsole()
+	if err != nil {
+		panic("unable to create new logger: " + err.Error())
+	}
+}
+
+
 func main()  {
-	fmt.Println("Hello World")
+	defer log.Stop()
+
+	log.Info("Bugsnag CLI Tool")
+
+	var commands struct {
+		UploadServer string `help:"Bugsnag On-Premise upload server URL"`
+		ApiKey    string	`help:"Bugsnag project API key"`
+		Upload       struct {
+			// shared options
+			Overwrite bool   	`help:"ignore existing upload with same version"`
+			Timeout   int    	`help:"seconds to wait before failing an upload request"`
+			Retries   int    	`help:"number of retry attempts before failing a request"`
+
+			// required options
+			Path 	  []string 	`arg:"" name:"path" help:"Path to directory to search" type:"path"`
+		} `cmd:"" help:"Upload files"`
+	}
+
+	ctx := kong.Parse(&commands)
+
+	// Check if path(s) provided are
+	for _,path := range commands.Upload.Path {
+		log.Info("Checking if %s is a valid file or path", path)
+		if !validation.ValidatePath(path) {
+			log.Error("%s is not valid file or path", path)
+			os.Exit(5)
+		}
+	}
+
+	switch ctx.Command() {
+	case "upload <path>":
+		log.Info("Starting Upload")
+		//fmt.Println(validation.ValidatePath())
+	default:
+		println(ctx.Command())
+	}
+
+	server.HelloWorld("test")
 }
