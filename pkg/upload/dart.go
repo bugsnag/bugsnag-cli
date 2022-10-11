35c05b033976f1275e6ed7e846a2723af9f7f8be
Josh Edney <josh.edney@smartbear.com>
2022-10-11 11:25:59 +0100

    format missing ios app path message

--- pkg/upload/dart.go
+++ pkg/upload/dart.go
@@ -191,7 +191,7 @@ func GetIosAppPath(symbolFile string) (string, error) {
 		}
 	}
 
-	return "", fmt.Errorf("unable to find IOS app path")
+	return "", fmt.Errorf("unable to find IOS app path, try adding --ios-app-path")
 }
 
 // DwarfDumpUuid - Gets the UUID from the Dwarf debug info of a file
