ffa514b20c6c1edb9e46a55756922f975d336266
Josh Edney <josh.edney@smartbear.com>
2022-12-06 16:16:45 +0000

    add create build pkg

--- pkg/build/create.go
+++ pkg/build/create.go
@@ -1,5 +1,18 @@
 package build
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/bugsnag/bugsnag-cli/pkg/log"
+	"github.com/bugsnag/bugsnag-cli/pkg/server"
+	"github.com/bugsnag/bugsnag-cli/pkg/utils"
+)
+
 type CreateBuild struct {
 	BuilderName  string            `help:"The name of the entity that triggered the build. Could be a user, system etc."`
 	Metadata     map[string]string `help:"Additional build information"`
@@ -7,4 +20,125 @@ type CreateBuild struct {
 	Provider     string            `help:"The name of the source control provider that contains the source code for the build."`
 	Repository   string            `help:"The URL of the repository containing the source code being deployed."`
 	Revision     string            `help:"The source control SHA-1 hash for the code that has been built (short or long hash)"`
+}
+
+type Payload struct {
+	ApiKey           string            `json:"apiKey,omitempty"`
+	BuilderName      string            `json:"builderName,omitempty"`
+	ReleaseStage     string            `json:"releaseStage,omitempty"`
+	SourceControl    SourceControl     `json:"sourceControl,omitempty"`
+	Metadata         map[string]string `json:"metadata,omitempty"`
+	AppVersion       string            `json:"appVersion,omitempty"`
+	AppVersionCode   string            `json:"appVersionCode,omitempty"`
+	AppBundleVersion string            `json:"appBundleVersion,omitempty"`
+}
+
+type SourceControl struct {
+	Provider   string `json:"provider,omitempty"`
+	Repository string `json:"repository,omitempty"`
+	Revision   string `json:"revision,omitempty"`
+}
+
+func ProcessBuildRequest(apiKey string, builderName string, releaseStage string, provider string, repository string, revision string, appVersion string, appVersionCode string, appBundleVersion string, metadata map[string]string, endpoint string) error {
+	builderName, err := SetBuilderName(builderName)
+
+	if err != nil {
+		log.Error("Failed to set builder name from system. "+err.Error(), 1)
+	}
+
+	repoInfo, err := GetRepoInfo(provider, repository, revision)
+
+	if err != nil {
+		log.Error("Failed to get source control information. "+err.Error(), 1)
+	}
+
+	payload := Payload{
+		ApiKey:       apiKey,
+		BuilderName:  builderName,
+		ReleaseStage: releaseStage,
+		SourceControl: SourceControl{
+			Provider:   repoInfo["provider"],
+			Repository: repoInfo["repository"],
+			Revision:   repoInfo["revision"],
+		},
+		Metadata:         metadata,
+		AppVersion:       appVersion,
+		AppVersionCode:   appVersionCode,
+		AppBundleVersion: appBundleVersion,
+	}
+
+	buildPayload, err := json.Marshal(payload)
+
+	if err != nil {
+		log.Error("Failed to create build information payload: " + err.Error(), 1)
+	}
+
+	prettyBuildPayload,_ := utils.PrettyPrintJson(string(buildPayload))
+	log.Info("Build information: \n" + prettyBuildPayload)
+
+	req, _ := http.NewRequest("POST", endpoint, bytes.NewBuffer(buildPayload))
+
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := server.SendRequest(req, 300)
+
+	if err != nil {
+		return fmt.Errorf("error sending file request: %w", err)
+	}
+
+	b, err := io.ReadAll(res.Body)
+
+	if strings.Contains(string(b), "Source control provider is missing") {
+		log.Warn("Source control provider is missing and could not be inferred. Please resend using one of: [github-enterprise, github, gitlab-onpremise, gitlab, bitbucket-server, bitbucket]. Request was still processed but source control information was ignored.")
+	}
+
+	if err != nil {
+		return fmt.Errorf("error reading body from response: %w", err)
+	}
+
+	if res.StatusCode != 200 {
+		return fmt.Errorf("%s : %s", res.Status, string(b))
+	}
+	return nil
+}
+
+func GetRepoInfo(repoProvider string, repoUrl string, repoHash string) (map[string]string, error) {
+	repoInfo := make(map[string]string)
+
+	if repoUrl == "" {
+		repoUrl, err := utils.GetRepoUrl()
+		if err != nil {
+			return nil, err
+		}
+		repoInfo["repository"] = repoUrl
+	} else {
+		repoInfo["repository"] = repoUrl
+	}
+
+	if repoProvider != "" {
+		repoInfo["provider"] = repoProvider
+	}
+
+	if repoHash == "" {
+		repoHash, err := utils.GetCommitHash()
+		if err != nil {
+			return nil, err
+		}
+		repoInfo["revision"] = repoHash
+	} else {
+		repoInfo["revision"] = repoHash
+	}
+
+	return repoInfo, nil
+}
+
+func SetBuilderName(name string) (string, error) {
+	if name == "" {
+		builder, err := utils.GetSystemUser()
+		if err != nil {
+			return name, err
+		}
+		return builder, nil
+	}
+	return name, nil
 }
