b3242b99ecebf7077e665626f705aef7e120a5a5
Josh Edney <josh.edney@smartbear.com>
2022-12-06 15:57:12 +0000

    Add pkg to get repo info from the system

---
+++ pkg/utils/get-repository-info.go
@@ -0,0 +1,62 @@
+package utils
+
+import (
+	"errors"
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+// GetRepoUrl - Gets the URl of a git repo.
+func GetRepoUrl() (string, error) {
+	gitLocation, err := exec.LookPath("git")
+
+	if err != nil {
+		return "", fmt.Errorf("unable to find git on system: %w", err)
+	}
+
+	cmd := exec.Command(gitLocation, "config", "--get", "remote.origin.url")
+
+	cmdOutput, err := cmd.CombinedOutput()
+
+	if err != nil {
+		return "", err
+	}
+
+	url, err := ParseGitUrl(strings.TrimSuffix(string(cmdOutput), "\n"))
+
+	if err != nil {
+		return "", err
+	}
+
+	return url, nil
+}
+
+//ParseGitUrl - Parses a git url to ensure that it is in HTTPS format
+func ParseGitUrl(url string) (string, error) {
+	if strings.Contains(url, "git@") {
+		url = strings.Replace(url, "git@", "https://", 1)
+		url = strings.Replace(url, ":", "/", 1)
+		url = strings.Replace(url, ":", "/", 1)
+		return url, nil
+	}
+	return url, errors.New("url error")
+}
+
+// GetCommitHash - Gets the commit hash from a repo
+func GetCommitHash() (string, error) {
+	gitLocation, err := exec.LookPath("git")
+
+	if err != nil {
+		return "", fmt.Errorf("unable to find git on system: %w", err)
+	}
+
+	cmd := exec.Command(gitLocation, "rev-parse", "HEAD")
+
+	cmdOutput, err := cmd.CombinedOutput()
+
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSuffix(string(cmdOutput), "\n"), nil
+}
