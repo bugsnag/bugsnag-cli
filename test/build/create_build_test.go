61d5a81a5777063052b49ac5bd6e66610e88e019
Josh Edney <josh.edney@smartbear.com>
2022-12-09 10:10:03 +0000

    remove dynamic unit tests

--- test/build/create_build_test.go
+++ test/build/create_build_test.go
@@ -25,52 +25,16 @@ func TestSetBuilderName(t *testing.T) {
 }
 
 func TestGettingRepoInfo(t *testing.T) {
-	t.Log("Test getting repo info map")
-	results, err := build.GetRepoInfo("", "", "")
-	if err != nil {
-		t.Error(err)
-	}
-	assert.Equal(t, map[string]string{
-		"repository": "https://github.com/bugsnag/bugsnag-cli",
-		"revision":   os.Getenv("GIT_SHA"),
-	}, results, "They should be the same")
-
-	t.Log("Test getting repo info map, only setting the provider")
-	results, err = build.GetRepoInfo("github", "", "")
-	if err != nil {
-		t.Error(err)
-	}
-	assert.Equal(t, map[string]string{
-		"repository": "https://github.com/bugsnag/bugsnag-cli",
-		"revision":   os.Getenv("GIT_SHA"),
-		"provider":   "github",
-	}, results, "They should be the same")
-
-	t.Log("Test getting repo info map, only setting the repo url")
-	results, err = build.GetRepoInfo("", "https://notgithub.com/bugsnag/bugsnag-cli", "")
-	if err != nil {
-		t.Error(err)
-	}
-	assert.Equal(t, map[string]string{
-		"repository": "https://notgithub.com/bugsnag/bugsnag-cli",
-		"revision":   os.Getenv("GIT_SHA"),
-	}, results, "They should be the same")
-
 	t.Log("Test getting repo info map, only setting the commit hash")
-	results, err = build.GetRepoInfo("", "", "0123456789")
-	if err != nil {
-		t.Error(err)
-	}
+	results := build.GetRepoInfo("", "", "0123456789")
+
 	assert.Equal(t, map[string]string{
-		"repository": "https://github.com/bugsnag/bugsnag-cli",
+		"repository": "git@github.com:bugsnag/bugsnag-cli",
 		"revision":   "0123456789",
 	}, results, "They should be the same")
 
 	t.Log("Test getting repo info map, passing all three variables")
-	results, err = build.GetRepoInfo("github", "https://notgithub.com/bugsnag/bugsnag-cli", "0123456789")
-	if err != nil {
-		t.Error(err)
-	}
+	results = build.GetRepoInfo("github", "https://notgithub.com/bugsnag/bugsnag-cli", "0123456789")
 	assert.Equal(t, map[string]string{
 		"repository": "https://notgithub.com/bugsnag/bugsnag-cli",
 		"revision":   "0123456789",
--- test/utils/repository_info_test.go
+++ test/utils/repository_info_test.go
@@ -1,28 +1,14 @@
 package utils_testing_test
 
 import (
-	"os"
 	"testing"
 
 	"github.com/bugsnag/bugsnag-cli/pkg/utils"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParseGitUrl(t *testing.T) {
-	t.Log("Testing parsing git ssh URL")
-	results := utils.ParseGitUrl("git@github.com:bugsnag/bugsnag-cli")
-	assert.Equal(t, "https://github.com/bugsnag/bugsnag-cli", results, "They should be the same")
-
-	t.Log("Testing parsing git HTTPS URL")
-	results = utils.ParseGitUrl("https://github.com/bugsnag/bugsnag-cli")
-	assert.Equal(t, "https://github.com/bugsnag/bugsnag-cli", results, "They should be the same")
-}
-
 func TestGetRepoUrl(t *testing.T) {
 	t.Log("Test getting repo url from system")
-	results, err := utils.GetRepoUrl()
-	if err != nil {
-		t.Error(err)
-	}
-	assert.Equal(t, "https://github.com/bugsnag/bugsnag-cli", results, "They should be the same")
+	results := utils.GetRepoUrl()
+	assert.Equal(t, "git@github.com:bugsnag/bugsnag-cli", results, "They should be the same")
 }
