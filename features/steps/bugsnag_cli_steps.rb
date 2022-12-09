ee3c53b06ff28b1fad3635a074d403dde0b256c2
Josh Edney <josh.edney@smartbear.com>
2022-12-09 09:53:30 +0000

    update E2E test

--- features/steps/bugsnag_cli_steps.rb
+++ features/steps/bugsnag_cli_steps.rb
@@ -1,7 +1,10 @@
 require 'rbconfig'
+require 'etc'
 
 os = RbConfig::CONFIG['host_os']
 arch = RbConfig::CONFIG['host_cpu']
+user = Etc.getlogin
+commit_hash = `git rev-parse HEAD`
 
 case
 when os.downcase.include?('windows_nt'), ENV['WSL_DISTRO_NAME'] != nil
@@ -41,4 +44,12 @@ end
 
 Then(/^I should see the no such file or directory error$/) do
   run_output.include?("error: <path>: stat /path/to/no/file: no such file or directory")
+end
+
+Then(/^the payload should match local information$/) do
+  run_output.include?("\"appVersion\": \"1.2.3\"")
+  run_output.include?("\"apiKey\": \"1234567890ABCDEF1234567890ABCDEF\"")
+  run_output.include?("\"builderName\": \"#{user}\"")
+  run_output.include?("\"revision\": \"#{commit_hash}\"")
+  run_output.include?("\"repository\": \"git@github.com:bugsnag/bugsnag-cli\"")
 end
