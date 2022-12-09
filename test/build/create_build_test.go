0ec69a2108f794e07d01d93b68c9ed55d66453e4
Josh Edney <josh.edney@smartbear.com>
2022-12-09 10:22:12 +0000

    update dynamic test

--- test/build/create_build_test.go
+++ test/build/create_build_test.go
@@ -26,7 +26,7 @@ func TestSetBuilderName(t *testing.T) {
 
 func TestGettingRepoInfo(t *testing.T) {
 	t.Log("Test getting repo info map, only setting the commit hash")
-	results := build.GetRepoInfo("", "", "0123456789")
+	results := build.GetRepoInfo("", "git@github.com:bugsnag/bugsnag-cli", "0123456789")
 
 	assert.Equal(t, map[string]string{
 		"repository": "git@github.com:bugsnag/bugsnag-cli",
