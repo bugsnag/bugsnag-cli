f4e9011b6dec9027ea8f8e5df43159ca291c78ba
Josh Edney <josh.edney@smartbear.com>
2023-01-05 15:22:49 +0000

    add android ndk root path checking

--- pkg/upload/android-ndk.go
+++ pkg/upload/android-ndk.go
@@ -1,7 +1,10 @@
 package upload
 
 import (
+	"fmt"
+	"github.com/bugsnag/bugsnag-cli/pkg/log"
 	"github.com/bugsnag/bugsnag-cli/pkg/utils"
+	"os"
 )
 
 type AndroidNdkMapping struct {
@@ -13,4 +16,34 @@ type AndroidNdkMapping struct {
 	ProjectRoot     string            `help:"path to remove from the beginning of the filenames in the mapping file" type:"path"`
 	VersionCode     string            `help:"Module version code"`
 	VersionName     string            `help:"Module version name"`
+}
+
+func ProcessAndroidNDK(paths []string, androidNdkRoot string, appManifestPath string, configuration string, projectRoot string, buildUuid string, versionCode string, versionName string, endpoint string, timeout int, retries int, overwrite bool, apiKey string, failOnUploadError bool) error {
+	androidNdkRoot, err := GetAndroidNDKRoot(androidNdkRoot)
+
+	if err != nil {
+		return err
+	}
+
+	log.Info("Using Android NDK Root: " + androidNdkRoot)
+
+	return nil
+}
+
+// GetAndroidNDKRoot - Returns a valid Android NDK root path
+func GetAndroidNDKRoot(path string) (string, error) {
+	if path == "" {
+		envValue, envPresent := os.LookupEnv("ANDROID_NDK_ROOT")
+		if envPresent {
+			path = envValue
+		} else {
+			return "", fmt.Errorf("unable to find ANDROID_NDK_ROOT")
+		}
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return path, fmt.Errorf(path + " does not exist on the system")
+	}
+
+	return path, nil
 }
