bfabda9118a6f33d8cad55a0cb07e9f1a520c6ce
Josh Edney <josh.edney@smartbear.com>
2023-01-09 08:54:15 +0000

    update build objcopy path

--- pkg/upload/android-ndk.go
+++ pkg/upload/android-ndk.go
@@ -121,19 +121,3 @@ func GetNdkVersion(path string) (int, error) {
 	}
 	return ndkIntVersion, nil
 }
-
-// GetVariants - Returns a list or variants from the merged_native_libs directory
-func GetVariants(path string) ([]string, error) {
-	var variants []string
-
-	fileInfo, err := ioutil.ReadDir(path)
-
-	if err != nil {
-		return nil, err
-	}
-	for _, file := range fileInfo {
-		variants = append(variants, file.Name())
-	}
-
-	return variants, nil
-}
