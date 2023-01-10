c67c1de23dd35300854ad66db81293dcff801937
Josh Edney <josh.edney@smartbear.com>
2023-01-10 11:19:41 +0000

    update android ndk tests

--- test/upload/android_ndk_test.go
+++ test/upload/android_ndk_test.go
@@ -9,30 +9,29 @@ import (
 
 func TestGetAndroidNDKRoot(t *testing.T) {
 	t.Log("Testing getting Android NDK Root")
-	results, err := upload.GetAndroidNDKRoot("~/Library/Android/sdk/ndk/23.1.7779620")
+	results, err := upload.GetAndroidNDKRoot("/opt/homebrew/share/android-commandlinetools/ndk/24.0.8215888")
 	if err != nil {
 		t.Error(err)
 	}
-	assert.Equal(t, "~/Library/Android/sdk/ndk/23.1.7779620", results, "The paths should match")
+	assert.Equal(t, "/opt/homebrew/share/android-commandlinetools/ndk/24.0.8215888", results, "The paths should match")
 }
-
 
 func TestBuildObjCopyPath(t *testing.T) {
 	t.Log("Testing building ObjCopy path")
-	results, err := upload.BuildObjCopyPath("~/Library/Android/sdk/ndk/23.1.7779620")
+	results, err := upload.BuildObjCopyPath("/opt/homebrew/share/android-commandlinetools/ndk/24.0.8215888")
 	if err != nil {
 		t.Error(err)
 	}
-	assert.Equal(t, 23, results, "The versions should match")
+	assert.Equal(t, "/opt/homebrew/share/android-commandlinetools/ndk/24.0.8215888/toolchains/llvm/prebuilt/darwin-x86_64/bin/llvm-objcopy", results, "The versions should match")
 }
 
 func TestGetNDKVersion(t *testing.T) {
 	t.Log("Testing getting Android NDK major version")
-	results, err := upload.GetNdkVersion("~/Library/Android/sdk/ndk/23.1.7779620")
+	results, err := upload.GetNdkVersion("/opt/homebrew/share/android-commandlinetools/ndk/24.0.8215888")
 	if err != nil {
 		t.Error(err)
 	}
-	assert.Equal(t, 23, results, "The versions should match")
+	assert.Equal(t, 24, results, "The versions should match")
 }
 
 func TestBuildVariantsList(t *testing.T) {
