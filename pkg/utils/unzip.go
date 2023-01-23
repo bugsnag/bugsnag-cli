88a6ef4d361a97c02daefa9b8ab97353c5ba8771
Josh Edney <josh.edney@smartbear.com>
2023-01-23 14:39:18 +0000

    add unzip util

---
+++ pkg/utils/unzip.go
@@ -0,0 +1,51 @@
+package utils
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func Unzip(path, outputPath string) error {
+	archive, err := zip.OpenReader(path)
+	if err != nil {
+		panic(err)
+	}
+	defer archive.Close()
+
+	for _, f := range archive.File {
+		filePath := filepath.Join(outputPath, f.Name)
+		if !strings.HasPrefix(filePath, filepath.Clean(outputPath)+string(os.PathSeparator)) {
+			return fmt.Errorf("invalid file path")
+		}
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(filePath, os.ModePerm)
+			continue
+		}
+
+		if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
+			return fmt.Errorf(err.Error())
+		}
+
+		outputPathFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return fmt.Errorf(err.Error())
+		}
+
+		fileInArchive, err := f.Open()
+		if err != nil {
+			return fmt.Errorf(err.Error())
+		}
+
+		if _, err := io.Copy(outputPathFile, fileInArchive); err != nil {
+			return fmt.Errorf(err.Error())
+		}
+
+		outputPathFile.Close()
+		fileInArchive.Close()
+	}
+	return nil
+}
