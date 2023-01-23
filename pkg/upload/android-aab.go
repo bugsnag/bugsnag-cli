fbac170e0301ed16f1888d8dfdd27abcd3232f29
Josh Edney <josh.edney@smartbear.com>
2023-01-23 10:13:58 +0000

    update type struct for aab

--- pkg/upload/android-aab.go
+++ pkg/upload/android-aab.go
@@ -9,7 +9,7 @@ type AndroidAabMapping struct {
 	BuildUuid     string            `help:"Module Build UUID"`
 	Configuration string            `help:"Build type, like 'debug' or 'release'"`
 	Path          utils.UploadPaths `arg:"" name:"path" help:"(required) Path to directory or file to upload" type:"path"`
-	ProjectRoot   utils.UploadPaths `help:"path to remove from the beginning of the filenames in the mapping file"`
+	ProjectRoot   string            `help:"path to remove from the beginning of the filenames in the mapping file"`
 	VersionCode   string            `help:"Module version code"`
 	VersionName   string            `help:"Module version name"`
 }
