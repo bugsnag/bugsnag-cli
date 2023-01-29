4a383e30f4b856e24abeacf7eeeb6a5860b6edad
Josh Edney <josh.edney@smartbear.com>
2023-01-29 18:06:26 +0000

    add process android functions

---
+++ pkg/android/process-objcopy.go
@@ -0,0 +1,25 @@
+package android
+
+import (
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+// Objcopy - Processes files using objcopy
+func Objcopy(objcopyPath string, file string) (string, error) {
+
+	objcopyLocation, err := exec.LookPath(objcopyPath)
+
+	if err != nil {
+		return "", err
+	}
+
+	outputFile := strings.ReplaceAll(file, filepath.Ext(file), ".sym.so")
+
+	cmd := exec.Command(objcopyLocation, "--compress-debug-sections=zlib", "--only-keep-debug", file, outputFile)
+
+	_, err = cmd.CombinedOutput()
+
+	return outputFile, nil
+}
