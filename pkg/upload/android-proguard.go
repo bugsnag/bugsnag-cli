b6fe0f3f5c6dc6630cb2dc7576771b70067e0dd1
Josh Edney <j.edney17@gmail.com>
2023-04-11 09:59:43 +0100

    update variant handling for proguard

--- pkg/upload/android-proguard.go
+++ pkg/upload/android-proguard.go
@@ -1,129 +1,125 @@
 package upload
 
 import (
-	"path/filepath"
-	"strconv"
-
+	"fmt"
 	"github.com/bugsnag/bugsnag-cli/pkg/android"
 	"github.com/bugsnag/bugsnag-cli/pkg/log"
 	"github.com/bugsnag/bugsnag-cli/pkg/server"
 	"github.com/bugsnag/bugsnag-cli/pkg/utils"
+	"path/filepath"
 )
 
 type AndroidProguardMapping struct {
 	ApplicationId   string            `help:"Module application identifier"`
-	AppManifestPath string            `help:"(required) Path to directory or file to upload" type:"path"`
+	AppManifestPath string            `help:"Path to app manifest file" type:"path"`
+	MappingPath     string            `help:"Path to app mapping file"`
 	BuildUuid       string            `help:"Module Build UUID"`
 	Configuration   string            `help:"Build type, like 'debug' or 'release'"`
-	Path            utils.UploadPaths `arg:"" name:"path" help:"(required) Path to directory or file to upload" type:"path"`
+	Path            utils.UploadPaths `arg:"" name:"path" help:"Path to directory or file to upload" type:"path" default:"."`
 	VersionCode     string            `help:"Module version code"`
 	VersionName     string            `help:"Module version name"`
 	DryRun          bool              `help:"Validate but do not upload"`
 }
 
-func ProcessAndroidProguard(paths []string, applicationId string, appManifestPath string, buildUuid string, configuration string, versionCode string, versionName string, endpoint string, timeout int, retries int, overwrite bool, apiKey string, failOnUploadError bool, dryRun bool) error {
+func ProcessAndroidProguard(paths []string, appManifestPath string, mappingPath string, buildUuid string, configuration string, appId string, versionCode string, versionName string, endpoint string, timeout int, retries int, overwrite bool, apiKey string, failOnUploadError bool, dryRun bool) error {
 
-	uploadFileOptions := make(map[string]map[string]string)
+	uploadFileOptions := make(map[string]string)
 
 	for _, path := range paths {
-		// Path is a directory
-		if utils.IsDir(path) {
-			mergedManifestsPath := filepath.Join(path, "build", "intermediates", "merged_manifests")
-			if utils.IsDir(mergedManifestsPath) {
-				variants, err := android.BuildVariantsList(mergedManifestsPath)
-				if err != nil {
-					log.Error(err.Error(), 1)
-				}
 
-				for _, variant := range variants {
-					uploadFileOptions[variant] = map[string]string{}
-					uploadFileOptions[variant]["androidManifestPath"] = filepath.Join(mergedManifestsPath, variant, "AndroidManifest.xml")
-					uploadFileOptions[variant]["mappingPath"] = filepath.Join(mergedManifestsPath, "..", "..", "outputs", "mapping", variant, "mapping.txt")
-				}
+		if apiKey == "" {
+			getApiKeyFromManifest = true
+		}
+
+		if appManifestPath == "" {
+			log.Info("Locating Android manifest")
+
+			if utils.FileExists(filepath.Join(path, "app", "build", "intermediates", "merged_manifests")) {
+				appManifestPath = filepath.Join(path, "app", "build", "intermediates", "merged_manifests")
 			} else {
-				log.Error("unable to find `merged_manifests` in "+path, 1)
+				return fmt.Errorf("unable to find AndroidManifest.xml. Please specify using `--app-manifest-path` ")
 			}
-			//	Path is a file
-		} else {
 
 			if configuration == "" {
-				log.Warn("`--configuration` missing from options for " + path)
-				log.Info("Skipping " + path)
-				continue
-			}
+				variants, err := android.BuildVariantsList(appManifestPath)
 
-			if appManifestPath == "" {
-				log.Warn("`--app-manifest-path` missing from options for " + path)
-				log.Info("Skipping " + path)
-				continue
-			}
-
-			if filepath.Base(path) == "mapping.txt" {
-				uploadFileOptions[configuration] = map[string]string{}
-				uploadFileOptions[configuration]["androidManifestPath"] = appManifestPath
-				uploadFileOptions[configuration]["mappingPath"] = path
-			} else {
-				log.Error(path+" is not a supported file. Please use `mapping.txt`", 1)
-			}
-		}
-	}
-
-	numberOfVariants := len(uploadFileOptions)
-
-	if numberOfVariants < 1 {
-		log.Info("No variants to process")
-		return nil
-	}
-
-	log.Info("Processing " + strconv.Itoa(numberOfVariants) + " variant(s)")
-
-	for variant, config := range uploadFileOptions {
-		log.Info("Processing mapping.txt for variant: " + variant)
-
-		log.Info("Gathering information from AndroidManifest.xml")
-
-		androidManifestData, err := android.ParseAndroidManifestXML(config["androidManifestPath"])
-
-		if err != nil {
-			return err
-		}
-
-		if buildUuid == "" {
-			for i := range androidManifestData.Application.MetaData.Name {
-				if androidManifestData.Application.MetaData.Name[i] == "com.bugsnag.android.BUILD_UUID" {
-					buildUuid = androidManifestData.Application.MetaData.Value[i]
+				if err != nil {
+					return err
 				}
+
+				if len(variants) > 1 {
+					fmt.Println(variants)
+					return fmt.Errorf("more than one variant found. Please specify using `--configuration`")
+				}
+
+				configuration = variants[0]
 			}
+
+			appManifestPath = filepath.Join(appManifestPath, configuration, "AndroidManifest.xml")
 		}
 
-		log.Info("Compressing " + config["mappingPath"])
-
-		outputFile, err := utils.GzipCompress(config["mappingPath"])
-
-		if err != nil {
-			return err
-		}
-
-		log.Info("Uploading debug information for " + config["mappingPath"])
-
-		uploadOptions := utils.BuildAndroidProguardUploadOptions(apiKey, androidManifestData.AppId, androidManifestData.VersionName, androidManifestData.VersionCode, buildUuid, overwrite)
-
-		fileFieldData := make(map[string]string)
-		fileFieldData["proguard"] = outputFile
-
-		requestStatus := server.ProcessRequest(endpoint, uploadOptions, fileFieldData, timeout)
-
-		if requestStatus != nil {
-			if numberOfVariants > 1 && failOnUploadError {
-				return requestStatus
+		if mappingPath == "" {
+			if utils.FileExists(filepath.Join(path, "app", "build", "outputs", "mapping", configuration, "mapping.txt")) {
+				mappingPath = filepath.Join(path, "app", "build", "outputs", "mapping", configuration, "mapping.txt")
 			} else {
-				log.Warn(requestStatus.Error())
+				return fmt.Errorf("unable to find mapping.txt. Please specify using `--mapping-path` ")
 			}
-		} else {
-			log.Success(config["mappingPath"] + " uploaded")
 		}
+
+		uploadFileOptions["androidManifestPath"] = appManifestPath
+		uploadFileOptions["mappingPath"] = mappingPath
 
 	}
 
+	log.Info("Processing mapping.txt for variant: " + configuration)
+
+	androidManifestData, err := android.ParseAndroidManifestXML(uploadFileOptions["androidManifestPath"])
+
+	if err != nil {
+		return err
+	}
+
+	if versionCode == "" {
+		versionCode = androidManifestData.VersionCode
+	}
+
+	if versionName == "" {
+		versionName = androidManifestData.VersionName
+	}
+
+	if appId == "" {
+		appId = androidManifestData.AppId
+	}
+
+	if buildUuid == "" {
+		for i := range androidManifestData.Application.MetaData.Name {
+			if androidManifestData.Application.MetaData.Name[i] == "com.bugsnag.android.BUILD_UUID" {
+				buildUuid = androidManifestData.Application.MetaData.Value[i]
+			}
+		}
+	}
+
+	log.Info("Compressing " + uploadFileOptions["mappingPath"])
+
+	outputFile, err := utils.GzipCompress(uploadFileOptions["mappingPath"])
+
+	if err != nil {
+		return err
+	}
+
+	log.Info("Uploading debug information for " + uploadFileOptions["mappingPath"])
+
+	uploadOptions := utils.BuildAndroidProguardUploadOptions(apiKey, appId, versionName, versionCode, buildUuid, overwrite)
+
+	fileFieldData := make(map[string]string)
+	fileFieldData["proguard"] = outputFile
+
+	requestStatus := server.ProcessRequest(endpoint, uploadOptions, fileFieldData, timeout)
+
+	if requestStatus != nil {
+		return requestStatus
+	} else {
+		log.Success(uploadFileOptions["mappingPath"] + " uploaded")
+	}
 	return nil
 }
