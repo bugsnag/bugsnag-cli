214db1c3513f68bfde0dd13c0f829c2a35bf57d7
Josh Edney <josh.edney@smartbear.com>
2023-06-08 15:01:04 +0100

    update package.json

--- install.js
+++ install.js
@@ -1,54 +1,58 @@
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
-const os = require("os");
-const cTable = require("console.table");
+const os = require('os');
 
+const { name, repository, version } = require('./package.json');
 
-const {name, repository, version} = require("./package.json");
-
-const error = msg => {
+const handleError = (msg) => {
     console.error(msg);
     process.exit(1);
 };
 
+// Remove the git prefix and suffix from the repository URL
+const removeGitPrefixAndSuffix = (input) => {
+    let result = input.replace(/^git\+/, '');
+    result = result.replace(/\.git$/, '');
+    return result;
+};
+
 const supportedPlatforms = [
     {
-        TYPE: "windows",
-        ARCHITECTURE: "x86_64",
-        ARTIFACT_NAME: "x86_64-windows-bugsnag-cli.exe",
-        BINARY_NAME: "bugsnag-cli.exe"
+        TYPE: 'windows',
+        ARCHITECTURE: 'x86_64',
+        ARTIFACT_NAME: 'x86_64-windows-bugsnag-cli.exe',
+        BINARY_NAME: 'bugsnag-cli.exe'
     },
     {
-        TYPE: "windows",
-        ARCHITECTURE: "i386",
-        ARTIFACT_NAME: "i386-windows-bugsnag-cli.exe",
-        BINARY_NAME: "bugsnag-cli.exe"
+        TYPE: 'windows',
+        ARCHITECTURE: 'i386',
+        ARTIFACT_NAME: 'i386-windows-bugsnag-cli.exe',
+        BINARY_NAME: 'bugsnag-cli.exe'
     },
     {
-        TYPE: "linux",
-        ARCHITECTURE: "x86_64",
-        ARTIFACT_NAME: "x86_64-linux-bugsnag-cli",
-        BINARY_NAME: "bugsnag-cli"
-
+        TYPE: 'linux',
+        ARCHITECTURE: 'x86_64',
+        ARTIFACT_NAME: 'x86_64-linux-bugsnag-cli',
+        BINARY_NAME: 'bugsnag-cli'
     },
     {
-        TYPE: "linux",
-        ARCHITECTURE: "i386",
-        ARTIFACT_NAME: "i386-linux-bugsnag-cli",
-        BINARY_NAME: "bugsnag-cli"
+        TYPE: 'linux',
+        ARCHITECTURE: 'i386',
+        ARTIFACT_NAME: 'i386-linux-bugsnag-cli',
+        BINARY_NAME: 'bugsnag-cli'
     },
     {
-        TYPE: "Darwin",
-        ARCHITECTURE: "x86_64",
-        ARTIFACT_NAME: "x86_64-macos-bugsnag-cli",
-        BINARY_NAME: "bugsnag-cli"
+        TYPE: 'Darwin',
+        ARCHITECTURE: 'x86_64',
+        ARTIFACT_NAME: 'x86_64-macos-bugsnag-cli',
+        BINARY_NAME: 'bugsnag-cli'
     },
     {
-        TYPE: "Darwin",
-        ARCHITECTURE: "arm64",
-        ARTIFACT_NAME: "arm64-macos-bugsnag-cli",
-        BINARY_NAME: "bugsnag-cli"
+        TYPE: 'Darwin',
+        ARCHITECTURE: 'arm64',
+        ARTIFACT_NAME: 'arm64-macos-bugsnag-cli',
+        BINARY_NAME: 'bugsnag-cli'
     }
 ];
 
@@ -56,38 +60,49 @@ const getPlatformMetadata = () => {
     const type = os.type();
     const architecture = os.arch();
 
-    for (let supportedPlatform of supportedPlatforms) {
-        if (
-            type === supportedPlatform.TYPE &&
-            architecture === supportedPlatform.ARCHITECTURE
-        ) {
+    for (const supportedPlatform of supportedPlatforms) {
+        if (type === supportedPlatform.TYPE && architecture === supportedPlatform.ARCHITECTURE) {
             return supportedPlatform;
         }
     }
 
-    error(
-        `Platform with type "${type}" and architecture "${architecture}" is not supported by ${name}.\nYour system must be one of the following:\n\n${cTable.getTable(
-            supportedPlatforms
+    const supportedPlatformsTable = supportedPlatforms.map((platform) => {
+        return {
+            Type: platform.TYPE,
+            Architecture: platform.ARCHITECTURE,
+            Artifact: platform.ARTIFACT_NAME
+        };
+    });
+
+    handleError(
+        `Platform with type "${type}" and architecture "${architecture}" is not supported by ${name}.\nYour system must be one of the following:\n\n${JSON.stringify(
+            supportedPlatformsTable,
+            null,
+            2
         )}`
     );
 };
 
-async function downloadBinaryFromGitHub(url, outputPath) {
+const downloadBinaryFromGitHub = async (downloadUrl, outputPath) => {
     try {
-        const response = await axios.get(url, { responseType: 'arraybuffer' });
+        const binDir = path.resolve(__dirname, '..', '.bin');
+        if (!fs.existsSync(binDir)) {
+            fs.mkdirSync(binDir, { recursive: true });
+        }
+
+        const response = await axios.get(downloadUrl, { responseType: 'arraybuffer' });
         const binaryData = response.data;
         fs.writeFileSync(outputPath, binaryData, 'binary');
         fs.chmodSync(outputPath, '755');
         console.log('Binary downloaded successfully!');
-    } catch (error) {
-        console.error('Error downloading binary:', error.message);
+    } catch (err) {
+        console.error('Error downloading binary:', err.message);
     }
-}
+};
 
 const platformMetadata = getPlatformMetadata();
-
-const binaryUrl = `${repository.url}/releases/download/${version}/${platformMetadata.ARTIFACT_NAME}`;
-
-const binaryOutputPath = path.join(__dirname, 'node_modules', '.bin', platformMetadata.BINARY_NAME);
+const repoUrl = removeGitPrefixAndSuffix(repository.url);
+const binaryUrl = `${repoUrl}/releases/download/v${version}/${platformMetadata.ARTIFACT_NAME}`;
+const binaryOutputPath = path.join(__dirname, '..', '.bin', platformMetadata.BINARY_NAME);
 
 downloadBinaryFromGitHub(binaryUrl, binaryOutputPath);
