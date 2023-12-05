f5c81884f1070feac4979b2783ddf242c121854d
Josh Edney <josh.edney@smartbear.com>
2023-12-05 09:40:01 +0000

    remove useless test

--- test/build/create_build_test.go
+++ test/build/create_build_test.go
@@ -2,17 +2,10 @@ package build
 
 import (
 	"github.com/bugsnag/bugsnag-cli/pkg/utils"
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-func TestSetBuilderName(t *testing.T) {
-	t.Log("Test getting the builders name from the system")
-	results := utils.GetSystemUser()
-	assert.Equal(t, os.Getenv("USER"), results, "They should be the same")
-}
 
 func TestGettingRepoInfo(t *testing.T) {
 	t.Log("Test getting repo info map, only setting the commit hash")
