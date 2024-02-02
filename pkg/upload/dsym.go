638e939e29d666561f9679f5d2c55a1b0f2c949d
Jason McCullough <jason.mccullough@smartbear.com>
2024-02-02 13:22:18 +0000

    initial upload options for dsym implemented with some initial validation logic

--- pkg/upload/dsym.go
+++ pkg/upload/dsym.go
@@ -1,6 +1,16 @@
 package upload
 
-import "github.com/bugsnag/bugsnag-cli/pkg/utils"
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+
+	"github.com/bugsnag/bugsnag-cli/pkg/ios"
+	"github.com/bugsnag/bugsnag-cli/pkg/log"
+	"github.com/bugsnag/bugsnag-cli/pkg/server"
+	"github.com/bugsnag/bugsnag-cli/pkg/utils"
+)
 
 type Dsym struct {
 	VersionName string      `help:"The version of the application."`
@@ -23,5 +33,57 @@ func ProcessDsym(
 	overwrite bool,
 	dryRun bool,
 ) error {
+
+	var buildSettings *ios.XcodeBuildSettings
+
+	for _, path := range paths {
+		uploadInfo, err := ios.ProcessPathValue(path, projectRoot)
+		if err != nil {
+			return err
+		}
+
+		// If the dsymPath is not fed in via <path>
+		if uploadInfo.DsymPath == "" {
+			buildSettings, err = ios.GetXcodeBuildSettings(path, scheme, projectRoot)
+			if err != nil {
+				return err
+			}
+
+			// Build the dsymPath from build settings
+			uploadInfo.DsymPath = buildSettings.ConfigurationBuildDir + "/" + buildSettings.DsymName + "/Contents/Resources/DWARF"
+
+			filesFound, _ := os.ReadDir(uploadInfo.DsymPath)
+			switch len(filesFound) {
+			case 0:
+				return errors.Errorf("No files found in location '%s'", uploadInfo.DsymPath)
+			case 1:
+				uploadInfo.DsymPath = filepath.Join(uploadInfo.DsymPath, filesFound[0].Name())
+			default:
+				return errors.Errorf("Multiple files found in location '%s'", uploadInfo.DsymPath)
+			}
+
+		}
+
+		uploadOptions, err := utils.BuildDsymUploadOptions(apiKey, versionName, dev, uploadInfo.ProjectRoot, overwrite)
+		if err != nil {
+			return err
+		}
+
+		if err != nil {
+			return err
+		}
+
+		fileFieldData := make(map[string]string)
+		fileFieldData["dsym"] = uploadInfo.DsymPath
+
+		err = server.ProcessFileRequest(endpoint+"/dsym", uploadOptions, fileFieldData, timeout, retries, uploadInfo.DsymPath, dryRun)
+
+		if err != nil {
+			return err
+		} else {
+			log.Success("Uploaded " + filepath.Base(uploadInfo.DsymPath))
+		}
+	}
+
 	return nil
 }
