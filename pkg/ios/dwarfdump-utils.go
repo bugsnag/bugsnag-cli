259df507fddb1a3a4a894b7baa51aba1b0943ffd
Jason McCullough <jason.mccullough@smartbear.com>
2024-02-09 12:22:09 +0000

    readability improvement for dwarfdump util function and cleanup of temp dir each iteration

--- pkg/ios/dwarfdump-utils.go
+++ pkg/ios/dwarfdump-utils.go
@@ -34,28 +34,29 @@ func GetDsymsForUpload(path string) (*[]*DwarfInfo, error) {
 	case 0:
 		return nil, errors.Errorf("No dSYM files found in expected location '%s'", path)
 	default:
-		if isDwarfDumpInstalled() {
-			for _, file := range filesFound {
-				if strings.HasSuffix(file.Name(), ".zip") {
-					log.Info("Attempting to unzip " + file.Name() + " before proceeding to upload")
-					tempDir, _ = utils.ExtractFile(filepath.Join(path, file.Name()), "dsym")
-
-					if tempDir != "" {
-						log.Info("Unzipped " + file.Name() + " to " + tempDir + " for uploading")
-						path = tempDir
-					} else {
-						log.Warn("Could not unzip " + file.Name() + " to a temporary directory, skipping")
-						// Silently remove the temp dir if one was created before continuing
-						removeTempDir(tempDir)
-						continue
-					}
-				}
-				dsymFiles = getDwarfFileInfo(path, file.Name())
-			}
-			removeTempDir(tempDir)
-		} else {
+		if !isDwarfDumpInstalled() {
 			return nil, errors.New("Unable to locate dwarfdump on this system.")
 		}
+
+		for _, file := range filesFound {
+			if strings.HasSuffix(file.Name(), ".zip") {
+				log.Info("Attempting to unzip " + file.Name() + " before proceeding to upload")
+				tempDir, _ = utils.ExtractFile(filepath.Join(path, file.Name()), "dsym")
+
+				if tempDir != "" {
+					log.Info("Unzipped " + file.Name() + " to " + tempDir + " for uploading")
+					path = tempDir
+				} else {
+					log.Warn("Could not unzip " + file.Name() + " to a temporary directory, skipping")
+					// Silently remove the temp dir if one was created before continuing
+					removeTempDir(tempDir)
+					continue
+				}
+			}
+			dsymFiles = getDwarfFileInfo(path, file.Name())
+			removeTempDir(tempDir)
+		}
+
 	}
 
 	return &dsymFiles, nil
