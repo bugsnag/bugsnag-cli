bbd3815ead5ce9cd65f33751515d9e7b0b5b1977
Jason McCullough <jason.mccullough@smartbear.com>
2024-02-21 12:17:22 +0000

    remove unused util

--- pkg/utils/log-utils.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-// DisplayBlankIfEmpty Displays a string value as <blank> if it's an empty string
-func DisplayBlankIfEmpty(value string) string {
-	if value == "" {
-		return "<blank>"
-	}
-	return value
-}
