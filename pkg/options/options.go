cc633beb8ba9a63ae4845cd634ab32c401adf3e1
Josh Edney <j.edney17@gmail.com>
2024-12-03 12:34:00 +0000

    fix cli options

--- pkg/options/options.go
+++ pkg/options/options.go
@@ -176,8 +176,9 @@ type CLI struct {
 		AndroidNdk         AndroidNdkMapping      `cmd:"" help:"Process and upload NDK symbol files for Android"`
 		AndroidProguard    AndroidProguardMapping `cmd:"" help:"Process and upload Proguard/R8 mapping files for Android"`
 		DartSymbol         DartSymbol             `cmd:"" help:"Process and upload symbol files for Flutter" name:"dart"`
-		XcodeBuild         XcodeBuild             `cmd:"" help:"Upload dSYMs for iOS build"`
+		XcodeBuild         XcodeBuild             `cmd:"" help:"Upload dSYMs for iOS from a build" aliases:"dsym"`
 		Dsym               XcodeBuild             `cmd:"" help:"(deprecated) Upload dSYMs for iOS"`
+		XcodeArchive       XcodeArchive           `cmd:"" help:"Upload dSYMs for iOS from a Xcarchive"`
 		Js                 Js                     `cmd:"" help:"Upload source maps for JavaScript"`
 		ReactNative        ReactNative            `cmd:"" help:"Upload source maps for React Native"`
 		ReactNativeAndroid ReactNativeAndroid     `cmd:"" help:"Upload source maps for React Native Android"`
