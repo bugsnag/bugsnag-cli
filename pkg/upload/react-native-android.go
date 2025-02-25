21433e12a04e185ea98488a26dbf852a55edb978
Josh Edney <j.edney17@gmail.com>
2025-02-25 15:11:06 +0000

    update variant Capitalisation for new manifest path

--- pkg/upload/react-native-android.go
+++ pkg/upload/react-native-android.go
@@ -3,6 +3,7 @@ package upload
 import (
 	"fmt"
 	"path/filepath"
+	"strings"
 
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
@@ -115,7 +116,7 @@ func ProcessReactNativeAndroid(options options.CLI, endpoint string, logger log.
 				androidOptions.Android.AppManifest = appManifestPathExpected
 				logger.Debug(fmt.Sprintf("Found app manifest at: %s", androidOptions.Android.AppManifest))
 			} else {
-				appManifestPathExpected = filepath.Join(buildDirPath, "intermediates", "merged_manifests", androidOptions.Android.Variant, "process"+androidOptions.Android.Variant+"Manifest", "AndroidManifest.xml")
+				appManifestPathExpected = filepath.Join(buildDirPath, "intermediates", "merged_manifests", strings.Title(androidOptions.Android.Variant), "process"+androidOptions.Android.Variant+"Manifest", "AndroidManifest.xml")
 				if utils.FileExists(appManifestPathExpected) {
 					androidOptions.Android.AppManifest = appManifestPathExpected
 					logger.Debug(fmt.Sprintf("Found app manifest at: %s", androidOptions.Android.AppManifest))
