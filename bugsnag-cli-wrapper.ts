944aa1de1bc8965a13c51ca1256c5fb5a86239c0
Ben Wilson <ben.wilson@smartbear.com>
2025-04-30 15:26:07 +0100

    convert cli-wrapper to typescript

---
+++ bugsnag-cli-wrapper.ts
@@ -0,0 +1,164 @@
+import { exec } from 'child_process'
+
+interface BaseOptions {
+    apiKey?: string
+    dryRun?: boolean
+    logLevel?: string
+    port?: number
+    failOnUploadError?: boolean
+    verbose?: boolean
+    overwrite?: boolean
+    retries?: number
+    timeout?: number
+}
+
+export interface BugsnagCreateBuildOptions extends BaseOptions {
+    autoAssignRelease?: boolean
+    buildApiRootUrl?: string
+    builderName?: string
+    metadata?: object
+    provider?: string
+    releaseStage?: string
+    repository?: string
+    revision?: string
+    versionName?: string
+    androidAab?: string
+    appManifest?: string
+    versionCode?: string
+    bundleVersion?: string
+} 
+
+export interface BugsnagUploadReactNativeOptions extends BaseOptions {
+    bundle?: string
+    codeBundleId?: string
+    sourceMap?: string
+    versionName?: string
+    androidAppManifest?: string
+    androidVariant?: string
+    androidVersionCode?: string
+    iosBundleVersion?: string
+    iosPlist?: string
+    iosScheme?: string
+    iosXcodeProject?: string
+}
+
+export interface BugsnagUploadiOSOptions extends BaseOptions {
+    bundle?: string
+    codeBundleId?: string
+    sourceMap?: string
+    versionName?: string
+    bundleVersion?: string
+    plist?: string
+    scheme?: string
+    xcodeProject?: string
+}
+
+export interface BugsnagUploadAndroidOptions extends BaseOptions {
+    bundle?: string
+    codeBundleId?: string
+    sourceMap?: string
+    versionName?: string
+    appManifest?: string
+    variant?: string
+    versionCode?: string
+}
+
+export interface BugsnagUploadJsOptions extends BaseOptions {
+    baseUrl?: string
+    bundle?: string
+    bundleUrl?: string
+    projectRoot?: string
+    sourceMap?: string
+    versionName?: string
+    codeBundleId?: string
+}
+
+/**
+ * Wrapper for Bugsnag CLI
+ */
+class BugsnagCLI {
+    /**
+     * Convert camelCase to kebab-case
+     */
+    static camelToKebab(str: string) {
+        return str.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase()
+    }
+
+    /**
+     * Execute a Bugsnag CLI command
+     */
+    static run(command: string, options = {}, target = '') {
+        return new Promise((resolve, reject) => {
+            // Convert the options keys from camelCase to kebab-case
+            const kebabCaseOptions = Object.entries(options)
+                .map(([key, value]) => {
+                    const kebabKey = BugsnagCLI.camelToKebab(key)
+                    if (typeof value === 'boolean' && value === true) {
+                        return `--${kebabKey}`
+                    } else if (typeof value !== 'boolean') {
+                        return `--${kebabKey}=${value}`
+                    }
+                    return ''
+                })
+                .filter(Boolean)
+                .join(' ')
+
+            const positionalArg = target ? `"${target}"` : ''
+            const cliCommand = `npx bugsnag-cli ${command} ${kebabCaseOptions} ${positionalArg}`.trim()
+
+            // Execute the command
+            exec(cliCommand, (error, stdout, stderr) => {
+                if (error) {
+                    const errorMessage = `Command failed: ${cliCommand}\n` +
+                        `Error: ${error.message}\n` +
+                        `${stdout.trim()}`
+                    reject(errorMessage)
+                } else {
+                    resolve(stdout.trim())
+                }
+            })
+        })
+    }
+
+    /**
+     * Upload sourcemaps to Bugsnag
+     * Provides nested methods for specific upload types.
+     */
+    static Upload = {
+
+        ReactNative: Object.assign(
+            (options: BugsnagUploadReactNativeOptions = {}, target = '') =>
+                BugsnagCLI.run('upload react-native', options, target), // Default ReactNative command
+            {
+                iOS: (options: BugsnagUploadiOSOptions = {}, target = '') =>
+                    BugsnagCLI.run('upload react-native-ios', options, target),
+            
+                Android: (options: BugsnagUploadAndroidOptions = {}, target = '') =>
+                    BugsnagCLI.run('upload react-native-android', options, target),
+            }
+        ),
+        Js: (options: BugsnagUploadJsOptions = {}, target = '') =>
+            BugsnagCLI.run('upload js', options, target),
+    }
+
+    /**
+     * Send build information to Bugsnag
+     */
+    static CreateBuild(options: BugsnagCreateBuildOptions = {}, target = '') {
+        return new Promise((resolve, reject) => {
+            try {
+                const output = BugsnagCLI.run('create-build', options, target)
+                if (output instanceof Promise) {
+                    output.then(resolve).catch(reject)
+                } else {
+                    resolve(output)
+                }
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+
+}
+
+export default BugsnagCLI
