574bf33b17d8d26e8b1266bcbef694f3f35dfcd0
Josh <j.edney17@gmail.com>
2025-06-03 12:13:00 +0100

    update unity android to upload unity line mappings

---
+++ pkg/unity/process-line-mappings.go
@@ -0,0 +1,39 @@
+package unity
+
+import (
+	"github.com/bugsnag/bugsnag-cli/pkg/log"
+	"github.com/bugsnag/bugsnag-cli/pkg/server"
+	"github.com/bugsnag/bugsnag-cli/pkg/utils"
+)
+import "github.com/bugsnag/bugsnag-cli/pkg/options"
+
+func UploadAndroidLineMappings(
+	lineMappingFile string,
+	apiKey string,
+	soBuildId string,
+	applicationId string,
+	versionName string,
+	versionCode string,
+	projectRoot string,
+	endpoint string,
+	options options.CLI,
+	logger log.Logger,
+) error {
+	fileFieldData := make(map[string]server.FileField)
+
+	uploadOptions, err := utils.BuildUnityAndroidLineMappingUploadOptions(apiKey, soBuildId, applicationId, versionName, versionCode, projectRoot, options.Upload.Overwrite)
+
+	if err != nil {
+		return err
+	}
+
+	fileFieldData["mappingFile"] = server.LocalFile(lineMappingFile)
+
+	err = server.ProcessFileRequest(endpoint+"/unity-line-mappings", uploadOptions, fileFieldData, lineMappingFile, options, logger)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
