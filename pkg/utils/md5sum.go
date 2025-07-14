6c701bd2529645b3971b6a09748cdf56c4e7ee21
Josh <j.edney17@gmail.com>
2025-07-14 17:07:39 +0100

    add md5sum function

---
+++ pkg/utils/md5sum.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"os"
+)
+
+func GetFileMD5(path string) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", hash.Sum(nil)), nil
+}
