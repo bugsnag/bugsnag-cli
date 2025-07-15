5abb66c1c0c852c668dff905495816c4174c0d7c
Josh <j.edney17@gmail.com>
2025-07-15 10:03:16 +0100

    remove GetMD5File functions

--- pkg/utils/md5sum.go
+++ pkg/utils/md5sum.go
@@ -3,32 +3,7 @@ package utils
 import (
 	"crypto/md5"
 	"fmt"
-	"io"
-	"os"
 )
-
-// GetFileMD5 calculates the MD5 checksum of the file at the specified path.
-//
-// Parameters:
-//   - path: File system path to the file.
-//
-// Returns:
-//   - A lowercase hexadecimal string representing the MD5 hash of the file contents.
-//   - An error if the file could not be read or the hash could not be computed.
-func GetFileMD5(path string) (string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return "", fmt.Errorf("failed to open file %q: %w", path, err)
-	}
-	defer file.Close()
-
-	hash := md5.New()
-	if _, err := io.Copy(hash, file); err != nil {
-		return "", fmt.Errorf("failed to compute MD5 for %q: %w", path, err)
-	}
-
-	return fmt.Sprintf("%x", hash.Sum(nil)), nil
-}
 
 // GetStringMD5 returns the MD5 checksum of the input string.
 //
